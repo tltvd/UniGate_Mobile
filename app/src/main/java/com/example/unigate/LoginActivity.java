9aa88b4e747175c239f62004b50b6015aaa79a68
tltvd <2448777B@gmail.com>
2023-05-15 21:20:30 +0600

    Обкаточная версия приложения 1.0.1

--- app/src/main/java/com/example/unigate/LoginActivity.java
+++ app/src/main/java/com/example/unigate/LoginActivity.java
@@ -1,36 +1,40 @@
 package com.example.unigate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.unigate.models.PackageData;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.unigate.models.PackageData;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.unigate.models.User;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-
+import java.security.cert.CertificateException;
 
 public class LoginActivity extends AppCompatActivity {
-
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button loginButton;
 
     public static User user;
-    private static final String SHARED_PREFS_FILE = "LoginPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,15 +57,10 @@ public class LoginActivity extends AppCompatActivity {
                 } else if (password.isEmpty()) {
                     // Проверка на пустое поле пароля
                     Toast.makeText(LoginActivity.this, "Введите пароль", Toast.LENGTH_SHORT).show();
-                } else if (isUserLoggedIn()) {
-                    // User is already logged in, skip the login process
-                    Toast.makeText(LoginActivity.this, "User is already logged in", Toast.LENGTH_SHORT).show();
-                    redirectToMainActivity();
                 } else if (isValidCredentials(username, password)) {
                     // Валидные учетные данные, выполняем вход
                     try {
                         loginUser(username, password);
-                        redirectToMainActivity();
                     } catch (NoSuchAlgorithmException e) {
                         throw new RuntimeException(e);
                     }
@@ -89,29 +88,12 @@ public class LoginActivity extends AppCompatActivity {
         user_check.setPassword(Security.hashPassword(password, "8fa985e47a9d6f1bd3bbb75427442f6b"));
         PackageData pd = new PackageData("SIGN_IN_MOBILE", user_check);
         connect(LoginActivity.this, pd);
-
-        // Save username and password to SharedPreferences
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS_FILE, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("username", username);
-        editor.putString("password", password);
-        editor.apply();
     }
 
-    private boolean isUserLoggedIn() {
-        // Check if the user is already logged in by checking SharedPreferences
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS_FILE, MODE_PRIVATE);
-        String username = sharedPreferences.getString("username", "");
-        String password = sharedPreferences.getString("password", "");
-
-        // Return true if both username and password are not empty
-        return !username.isEmpty() && !password.isEmpty();
-    }
-
-    public void redirectToMainActivity() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+    public static void redirectToMainActivity(Activity activity) {
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
     }
 
     public static void connect(Activity activity, PackageData pd) {
@@ -119,7 +101,23 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Socket socket = new Socket("192.168.1.123", 3489);
+                    // Загрузка SSL-сертификата
+                    char[] keystorePassword = "iitu2019".toCharArray(); // Пароль для доступа к хранилищу ключей
+                    KeyStore keystore = KeyStore.getInstance("JKS");
+                    keystore.load(LoginActivity.class.getResourceAsStream("/keystore.jks"), keystorePassword);
+
+                    // Создание менеджера ключей
+                    TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                    tmf.init(keystore);
+
+                    // Создание SSL-контекста
+                    SSLContext sslContext = SSLContext.getInstance("TLS");
+                    sslContext.init(null, tmf.getTrustManagers(), null);
+
+                    // Создание SSL-сокета
+                    SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                    SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket("192.168.1.123", 3489);
+
                     ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
                     ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
 
@@ -127,10 +125,12 @@ public class LoginActivity extends AppCompatActivity {
                         outputStream.writeObject(pd);
                         PackageData infoFromServer = (PackageData) inputStream.readObject();
                         user = infoFromServer.getUser();
-                    } else if (pd.getOperationType().equals("OPEN_DOOR_MOBILE")) {
-                        outputStream.writeObject(pd);
-                        PackageData infoFromServer = (PackageData) inputStream.readObject();
-                        MainActivity.door = infoFromServer.getDoor();
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                redirectToMainActivity(activity);
+                            }
+                        });
                     }
 
                     inputStream.close();
@@ -155,6 +155,14 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     });
                 } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (CertificateException e) {
+                    throw new RuntimeException(e);
+                } catch (KeyStoreException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                } catch (KeyManagementException e) {
                     throw new RuntimeException(e);
                 }
             }
